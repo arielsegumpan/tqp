46d20eccfa152838222a6d6de29de613d0c95e7d
arielsegumpan <arielsegumpan31@gmail.com>
2024-02-05 09:32:21 +0800

    update pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -50,6 +50,8 @@ $(document).ready(() => {
 
     let nat1 = $('#nat-carous1');
     let nat2 = $('#nat-carous2');
+    let bird1 = $('#bird-carous1');
+    
     // carousel slider
     nat1.owlCarousel({
         touchDrag  : false,
@@ -102,7 +104,32 @@ $(document).ready(() => {
             }
         }
     });
-
+    bird1.owlCarousel({
+        touchDrag  : false,
+        mouseDrag  : false,
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: false,
+        center:true,
+        dots:false,
+        autoplayTimeout: 10000,
+        autoplay:true,
+        fluidSpeed: true,
+        autoplaySpeed: 6000,
+        smartSpeed:3000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
     // control next and prev button sa carousel speed 3 sec
     $('#nextBtn-1').click(function() {
         nat1.trigger('next.owl.carousel',[3000]);
@@ -115,6 +142,13 @@ $(document).ready(() => {
     })
     $('#prevBtn-2').click(function() {
         nat2.trigger('prev.owl.carousel', [3000]);
+    })
+
+    $('#nextBirdBtn-1').click(function() {
+        bird1.trigger('next.owl.carousel',[3000]);
+    })
+    $('#prevBirdBtn-1').click(function() {
+        bird1.trigger('prev.owl.carousel', [3000]);
     })
 
 
